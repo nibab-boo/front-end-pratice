c8b4320f9b3e5bb20d0d0286aa523ca8dfcb13c8
Babin Bohora <bohorababin1@gmail.com>
2022-05-23 23:47:30 +0900

    changing color for navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,20 @@
 import React from 'react';
+
 const Header = () => {
+  
+  window.addEventListener("scroll", (e)=> {
+    const navbar = document.querySelector(".navbar.navbar-expand-lg.navbar-light.fixed-top")
+    if (e.currentTarget.scrollY > 20) {
+      const opacity = e.currentTarget.innerHeight / e.currentTarget.scrollY
+      navbar.style.background = `rgba(255, 255, 255, ${ 0.2 / opacity })`;
+    } else {
+      navbar.style.background = "initial";
+    }
+  })
+
   return (
     <header>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
         <div className="container-md fw-bold">
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
