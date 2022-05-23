13daa08f518bc269a79676e60d9f5293f643391e
Babin Bohora <bohorababin1@gmail.com>
2022-05-23 21:20:18 +0900

    video section

---
+++ src/components/Video.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import ad from './../assets/ad.mp4'
+import bag from './../assets/bag.png'
+
+const Video = () => {
+  const videoStopper = (e) => {
+    console.log(e.currentTarget.currentTime)
+    if (e.currentTarget.currentTime >= 58){
+      // document.querySelector("div#myVideo").classList.remove('opacity-0');
+      e.currentTarget.pause();
+      document.querySelector('.content').classList.remove("opacity-0");
+      e.currentTarget.classList.add('opacity-0');
+    }
+  }
+
+  return (
+    <>
+      <div style={{backgroundImage: `url(${bag})`, backgroundPosition: "center", backgroundSize: "cover"}} alt="bag" id="myVideo">
+        <video autoPlay muted id="myVideo" onTimeUpdate={ (e) => videoStopper(e)}>
+          <source src={ad} type="video/mp4" />
+        </video>
+        <div className="content mt-5 mt-md-0 opacity-0 text-center">
+          <h3 className=' d-block'>THE BAG.v22</h3>
+          <a href='#' className='nav-link mt-3 fs-6 p-3 border' style={{color: "inherit"}}>PREORDER NOW!</a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Video;
